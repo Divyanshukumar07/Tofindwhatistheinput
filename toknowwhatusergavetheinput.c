a6822ba7f7b1117d58724ae1759033be7ac73007
Divyanshukumar07 <divyanshu912266@gmail.com>
2024-09-20 22:16:56 +0530

    Add files via upload

---
+++ toknowwhatusergavetheinput.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    int a,n;
+    char sentence[100];
+    printf("enter whatever u want: ");
+    scanf("%s",&sentence);
+    if ("sentence")
+    {
+        printf("You entered: %s\n", sentence);
+        
+    }
+    else
+    {
+        scanf("%d",&a);
+        printf("you entered: %d",a);
+    }
+}
